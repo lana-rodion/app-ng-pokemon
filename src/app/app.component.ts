33b4a565671c248daa216cf0881d674bdd18bcca
lana rodion <lana.rodion@gmail.com>
2021-10-16 01:12:41 +0200

    Added app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'pokemon-app',
-	template: `<h1>Hello {{name}}</h1>`,
-	templateUrl: './app/app.component.html'
+	template: `<h1>Hello {{name}}</h1>`
 })
-export class AppComponent { }
+export class AppComponent { name = 'Angular'; }
