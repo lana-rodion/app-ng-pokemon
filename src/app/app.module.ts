46f1741aa200fcc657cf1dbf666a5a673543d739
lana rodion <lana.rodion@gmail.com>
2021-10-17 15:52:42 +0200

    Added @Directive and @Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 
 @NgModule({
 	imports: [BrowserModule],
-	declarations: [AppComponent],
+	declarations: [AppComponent, BorderCardDirective, PokemonTypeColorPipe],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
