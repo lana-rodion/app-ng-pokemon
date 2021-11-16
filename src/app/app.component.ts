089e45c0c744995359a1c582b4b818aea1962c8d
lana rodion <lana.rodion@gmail.com>
2021-11-16 19:51:12 +0100

    Added Title Service into app.component and app.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// On import le Service Title
+import { Title } from '@angular/platform-browser';
 
 @Component({
 	selector: 'pokemon-app',
 	templateUrl: './app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+	public constructor(private titleService: Title) {}
+
+	ngOnInit() {
+		this.titleService.setTitle("Mon Angular App");
+	}
+}
--- src/dist/app/app.component.js
+++ src/dist/app/app.component.js
@@ -5,17 +5,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppComponent = void 0;
 var core_1 = require("@angular/core");
+// On import le Service Title
+var platform_browser_1 = require("@angular/platform-browser");
 var AppComponent = /** @class */ (function () {
-    function AppComponent() {
+    function AppComponent(titleService) {
+        this.titleService = titleService;
     }
+    AppComponent.prototype.ngOnInit = function () {
+        this.titleService.setTitle("Mon Angular App");
+    };
     AppComponent = __decorate([
         (0, core_1.Component)({
             selector: 'pokemon-app',
             templateUrl: './app/app.component.html'
-        })
+        }),
+        __metadata("design:paramtypes", [platform_browser_1.Title])
     ], AppComponent);
     return AppComponent;
 }());
